6862a23f1eac1540cb030a229d3edb73404480f6
Safia Hirsi <safiahirsi@Sadias-MacBook-Air.local>
2020-01-13 09:47:42 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,37 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{ rank: "queen"	,
+suit:"hearts" ,
+cardImage: "images/queen-of-hearts.png"
+},
+{ rank:"queen"	,
+suit:"diamonds" ,
+cardImage:"images/queen-of-diamonds.png" 
+},
+{ rank: "king",
+suit: "hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{ rank: "king",
+suit:"diamonds" ,
+cardImage:"images/king-of-diamonds.png" ,
+}
+];
+
+
+
 let cardsInPlay = [];
 function checkForMatch (){
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		console.log ("You found a match!");
-} else {
-	console.log("Sorry, try again.");} 
+} else  if (cardsInPlay.length === 2 )
+	{console.log("Sorry, try again.");} 
 }
 let cardId = cards.length;
 function flipCard (cardId) {
- cardsInPlay.push (cards[cardId])
- console.log ("User flipped " + cards[cardId]);
+ cardsInPlay.push (cards[cardId].rank)
+ console.log ("User flipped " + cards[cardId].rank);
+ console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
  checkForMatch();
 	}
 
