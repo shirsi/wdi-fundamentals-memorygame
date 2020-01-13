adc9bc3d4c22d6487bbe337bf38cdea4a7843be1
Safia Hirsi <safiahirsi@Sadias-MacBook-Air.local>
2020-01-13 06:40:43 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,13 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour= "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen ")
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king ")
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) 
+	{
+		alert("You found a match!"); }
+		 else {alert("Sorry, try again.");
+		}
